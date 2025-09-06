7d67829fa03c32aab1d65ed69f6351c57b6ca363
KxllMach <135055683+KxllMach@users.noreply.github.com>
2025-09-06 19:51:01 +0530

    HeirarchyChange

--- src/constants/models.jsx
+++ src/constants/models.jsx
@@ -1,9 +1,9 @@
 export const AI_MODELS = [
   {
-    id: 'model.onnx',
-    name: 'SwinIR',
-    description: 'A powerful Transformer-based model for high-quality, general-purpose upscaling.',
-    url: 'https://huggingface.co/KxllMach/Upscaler-Models/resolve/main/model.onnx'
+    id: 'real_esrgan_x4_fp16.onnx',
+    name: 'Real-ESRGAN',
+    description: 'A fast and efficient model for general use, providing a good balance between speed and quality.',
+    url: 'https://huggingface.co/KxllMach/Upscaler-Models/resolve/main/real_esrgan_x4_fp16.onnx'
   },
   {
     id: 'RealESRGAN_x4plus_anime_4B32F.onnx',
@@ -12,10 +12,10 @@ export const AI_MODELS = [
     url: 'https://huggingface.co/KxllMach/Upscaler-Models/resolve/main/RealESRGAN_x4plus_anime_4B32F.onnx'
   },
   {
-    id: 'real_esrgan_x4_fp16.onnx',
-    name: 'Lite',
-    description: 'Lightweight model for quick processing with moderate quality improvements.',
-    url: 'https://huggingface.co/KxllMach/Upscaler-Models/resolve/main/real_esrgan_x4_fp16.onnx'
+    id: 'model.onnx',
+    name: 'SwinIR',
+    description: 'A powerful Transformer-based model for the highest quality, general-purpose upscaling.',
+    url: 'https://huggingface.co/KxllMach/Upscaler-Models/resolve/main/model.onnx'
   },
 ];
 
